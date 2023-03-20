9f102e2b10b09f82d24de10bcf56ce4ee72008d9
Graveyard7102 <119314224+Graveyard7102@users.noreply.github.com>
2023-03-20 17:44:23 +0500

    Калькулятор
    Калькулятор с написанными кнопками

--- ccccalc.py
+++ ccccalc.py
@@ -91,8 +91,6 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.ythth()
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
